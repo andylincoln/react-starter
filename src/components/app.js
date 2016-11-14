d96e01cae0fb4dc01ec02adb71b7b2ba385cfc32
Andy Lincoln <andrewlincoln11@gmail.com>
2016-11-14 16:58:58 -0500

    Removing references to original project

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>maark-webapp</h1>
-      </div>
+        <div>
+          <h1>react-starter</h1>
+        </div>
     );
   }
 }
