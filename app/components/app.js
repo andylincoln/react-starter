58efd94d8de36b311eb87641d067d589adcc6298
Andy Lincoln <andrewlincoln11@gmail.com>
2016-12-07 12:47:35 -0500

    Replacing dummy content with Header/Footer components

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 export default class App extends Component {
   render() {
     return (
-        <div>
-          <h1 className="app-name">react-starter</h1>
-          <p>{process.env.NODE_ENV}</p>
-        </div>
+      <div>
+        <Header></Header>
+        <Footer></Footer>
+      </div>
     );
   }
 }
