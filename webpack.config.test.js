e233d74983f25bc464426013fdefb7a890a80660
Andy Lincoln <andrewlincoln11@gmail.com>
2016-12-07 13:27:24 -0500

    Switching over to SASS for building

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -4,14 +4,14 @@ const merge = require('webpack-merge');
 const validate = require("webpack-validator");
 
 const parts = require('./webpack.parts');
-const style = path.join(parts.style_dir, "app.css");
+const style = path.join(parts.style_dir, "app.scss");
 
 let config = merge(
   {
     devtool: "inline-source-map"
   },
   parts.common,
-  parts.setupCSS(style),
+  parts.setupSASS(style),
   parts.compile_app(
     parts.app_dir,
     (/node_modules/) // Exclude only the node_modules dir, include the test dir
