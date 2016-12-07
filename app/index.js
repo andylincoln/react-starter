e3b6557c29ebc6cd634917629925b8ac59147f24
Andy Lincoln <andrewlincoln11@gmail.com>
2016-12-07 12:36:18 -0500

    Referencing index.html template for hotloading

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import index from '../index.html';
 import App from './components/app';
 import reducers from './reducers';
 
