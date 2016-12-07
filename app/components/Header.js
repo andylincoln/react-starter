eb56ccb07060aeaa561bdc7818635eeb3cae08d4
Andy Lincoln <andrewlincoln11@gmail.com>
2016-12-07 13:33:28 -0500

    Navigation Component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 
 export default class Header extends Component {
   render() {
     return (
       <header role="banner">
-        
+        <Nav></Nav>
       </header>
     );
   }
